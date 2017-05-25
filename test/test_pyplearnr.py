a4c02f97b2c0a9b2c25e91567ec4face1229596a
Christopher Shymansky <cmshymansky@gmail.com>
2017-05-25 14:29:35 -0700

    Corrected a bug in the AugmentedTestCase code.

--- test/test_pyplearnr.py
+++ test/test_pyplearnr.py
@@ -8,27 +8,20 @@ import unittest
 
 class AugmentedTestCase(unittest.TestCase):
     """
-    unittest.TestCase class with a helper method
+    unittest.TestCase class with an extra helper method for comparing expected
+    and actual errors
     """
     def assert_with_messsage(self, msg, func, args, kwargs):
-        print 'yay'
         try:
             func(*args, **kwargs)
-            self.assertFail()
+            # self.assertFail()
         except Exception as inst:
             self.assertEqual(inst.message, msg)
 
-class NestedKFoldCrossValidationTestCase(unittest.TestCase):
+class NestedKFoldCrossValidationTestCase(AugmentedTestCase):
     """
     Tests NestedKFoldCrossValidation class
     """
-    def assert_with_messsage(self, msg, func, args, kwargs):
-        try:
-            func(*args, **kwargs)
-            self.assertFail()
-        except Exception as inst:
-            self.assertEqual(inst.message, msg)
-
     def test_init_outer_loop_fold_count_zero(self):
         ############### Test initialization inputs ###############
         msg = "The outer_loop_fold_count" \
@@ -92,41 +85,41 @@ class NestedKFoldCrossValidationTestCase(unittest.TestCase):
         self.assert_with_messsage(msg, ppl.NestedKFoldCrossValidation,
                                   [], kwargs)
 
-    # def test_init_outer_loop_split_seed_negative(self):
-    #     msg = "The " \
-    #         "outer_loop_split_seed keyword argument, dictating how the data "\
-    #         "is split into folds for the outer loop, must be an integer."
-    #
-    #     kwargs = {
-    #         'outer_loop_split_seed': -5
-    #     }
-    #
-    #     self.assert_with_messsage(msg, ppl.NestedKFoldCrossValidation,
-    #                               [], kwargs)
-    #
-    # def test_init_inner_loop_split_seed_zero(self):
-    #     msg = "The " \
-    #         "inner_loop_split_seed keyword argument, dictating how the data "\
-    #         "is split into folds for the inner loop, must be an integer."
-    #
-    #     kwargs = {
-    #         'inner_loop_split_seed': 0
-    #     }
-    #
-    #     self.assert_with_messsage(msg, ppl.NestedKFoldCrossValidation,
-    #                               [], kwargs)
-    #
-    # def test_init_inner_loop_split_seed_negative(self):
-    #     msg = "The " \
-    #         "inner_loop_split_seed keyword argument, dictating how the data "\
-    #         "is split into folds for the inner loop, must be an integer."
-    #
-    #     kwargs = {
-    #         'inner_loop_split_seed': -5
-    #     }
-    #
-    #     self.assert_with_messsage(msg, ppl.NestedKFoldCrossValidation,
-    #                               [], kwargs)
+    def test_init_outer_loop_split_seed_negative(self):
+        msg = "The " \
+            "outer_loop_split_seed keyword argument, dictating how the data "\
+            "is split into folds for the outer loop, must be an integer."
+
+        kwargs = {
+            'outer_loop_split_seed': -5
+        }
+
+        self.assert_with_messsage(msg, ppl.NestedKFoldCrossValidation,
+                                  [], kwargs)
+
+    def test_init_inner_loop_split_seed_zero(self):
+        msg = "The " \
+            "inner_loop_split_seed keyword argument, dictating how the data "\
+            "is split into folds for the inner loop, must be an integer."
+
+        kwargs = {
+            'inner_loop_split_seed': 0
+        }
+
+        self.assert_with_messsage(msg, ppl.NestedKFoldCrossValidation,
+                                  [], kwargs)
+
+    def test_init_inner_loop_split_seed_negative(self):
+        msg = "The " \
+            "inner_loop_split_seed keyword argument, dictating how the data "\
+            "is split into folds for the inner loop, must be an integer."
+
+        kwargs = {
+            'inner_loop_split_seed': -5
+        }
+
+        self.assert_with_messsage(msg, ppl.NestedKFoldCrossValidation,
+                                  [], kwargs)
 
 if __name__ == '__main__':
     unittest.main()
