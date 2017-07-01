2f395aad41c363c1f9a8bd207bca70b45585e349
Christopher Shymansky <cmshymansky@gmail.com>
2017-07-01 00:35:14 -0700

    About to remove all distribution files to see if I can get this to work.

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "pyplearnr",
-    version = "1.0.9",
+    version = "1.0.10",
     author = "Christopher Shymansky",
     author_email = "CMShymansky@gmail.com",
     description = ("Pyplearnr is a tool designed to easily and more " \
                    "elegantly build, validate (nested k-fold cross-validation" \
                    "), and test scikit-learn pipelines."),
-    license = "ALv2",
+    license = "OSI Approved :: Apache Software License",
     keywords = "scikit-learn pipeline k-fold cross-validation model selection",
     url = "http://packages.python.org/pyplearnr",
     packages=['pyplearnr', 'test'],
@@ -30,6 +26,6 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-        "License :: ALv2",
+        "License :: OSI Approved :: Apache Software License",
     ],
 )
